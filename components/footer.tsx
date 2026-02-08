cacf42b24a6ea72a187a924b21102ca305a1b933
Miika Holmlund <miika.holmlund@gmail.com>
2026-02-08 09:27:23 +0200

    add privacy and license pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,19 @@
 import Link from "next/link"
 import styles from "./footer.module.css"
 import Image from "next/image"
-import GithubLogoDark from "../public/github-logo-dark.svg";
-import GithubLogoWhite from "../public/github-logo-white.svg";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerRow}>
-        <Link className={styles.footerItem} href="/">Privacy</Link>
+        <Link className={styles.footerItem} href="/privacy">Privacy</Link>
         <Link className={styles.footerItem} href="https://github.com/maholmlund/tiuku">
           <picture>
             <source srcSet="/github-logo-white.svg" media="(prefers-color-scheme: dark)" />
             <Image src="/github-logo-dark.svg" alt="github logo" width={64} height={64} />
           </picture>
         </Link>
-        <Link className={styles.footerItem} href="/">License</Link>
+        <Link className={styles.footerItem} href="https://github.com/maholmlund/tiuku/blob/main/LICENSE">License</Link>
       </div>
       <div className={styles.footerRow}>
         <p className={styles.footerItem}>With ❤️ from Finland</p>
