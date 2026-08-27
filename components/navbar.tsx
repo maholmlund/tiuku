8018a9af5e701bf8294484136b275d9b6fbc6721
Miika Holmlund <miika.holmlund@gmail.com>
2026-08-27 12:24:42 +0300

    fix console error when navigating to front page from new poll page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,11 +3,13 @@ import styles from "./navbar.module.css"
 import { ReactNode } from "react"
 
 export default function NavBar({ children }: { children?: ReactNode }) {
+  // Using an a-tag in the navbar instead of a Link tag fixes a console error when
+  // navigating from the front page to the poll page and then back to the front page.
   return (
     <nav className={styles.navbar}>
-      <Link href="/">
+      <a href="/">
         <h1>Tiuku</h1>
-      </Link>
+      </a>
       <div className={styles.linkGroup}>
         <div className={styles.navBarItem}>
           {children}
