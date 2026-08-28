4170a0c4002d790fe5f1d5df49385dd64487a9b4
Miika Holmlund <miika.holmlund@gmail.com>
2026-08-28 10:51:09 +0300

    create front page texts

--- app/page.tsx
+++ app/page.tsx
@@ -6,31 +6,34 @@ import { IconChevronDown } from "@tabler/icons-react";
 
 export default function Home() {
   return (
-    <div className={styles.centered}>
-      <div className={styles.landingSection}>
-        <h1>Tiuku</h1>
-        <p className={styles.explanationParagraph}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi totam, quaerat debitis recusandae vitae rem! Minima voluptates, necessitatibus optio aspernatur maxime suscipit magnam vitae itaque ullam quisquam laboriosam nam repellendus quae, quod quam in iusto rerum quis explicabo. Cum, veritatis!</p>
-        <p className={styles.explanationParagraph}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi totam, quaerat debitis recusandae vitae rem! Minima voluptates, necessitatibus optio aspernatur maxime suscipit magnam vitae itaque ullam quisquam laboriosam nam repellendus quae, quod quam in iusto rerum quis explicabo. Cum, veritatis!</p>
-        <Link href={"#bottom"}>
-          <IconChevronDown size={48} color="var(--text3)" className={styles.scrollDown} />
+    <>
+      <div className={styles.centered}>
+        <div className={styles.landingSection}>
+          <h1>Tiuku</h1>
+          <p className={styles.explanationParagraph}>This is a demo deployment. Please use only test data.</p>
+        </div>
+      </div>
+      <p className={styles.explanationParagraph}>Tiuku is an application that allows you to create date-based polls. You can select a time interval between 1 and 30 days and have up to 10 answers. Polls are automatically deleted 30 days after creation.</p>
+      <p className={styles.explanationParagraph}>Tiuku is an end-to-end encrypted poll service. When creating a new poll, the browser creates an AES-128 encryption key, creates an empty poll, encrypts it, and sends it to the server. The server only stores the encrypted data, the UUID of the poll, and the date of its creation. The link of the poll first contains the UUID of the poll, followed by the encryption key in the anchor tag. The encryption key is not transferred to the server.</p>
+      <p className={styles.explanationParagraph}>When opening a poll, the browser requests the encrypted data from the server. It then decrypts the data with the encryption key from the anchor tag. When the poll is modified, the data is re-encrypted and sent to the server, which stores it only in an encrypted format.</p>
+      <div className={styles.cardContainer} >
+        <Card>
+          <h1>No Accounts</h1>
+          <p>There are no accounts. Just create a poll and share it with your friends.</p>
+        </Card>
+        <Card>
+          <h1>Free?</h1>
+          <p>This demo deployment is free until my MongoDB Atlas free tier limit of 512MB runs out :)</p>
+        </Card>
+      </div>
+      <div className={styles.centered}>
+        <Link href="/new">
+          <AccentButton>
+            New Poll
+          </AccentButton>
         </Link>
       </div>
-      <div className={styles.cardContainer} id="bottom">
-        <Card>
-          <h1>Lorem</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id eos libero temporibus. Voluptatem atque ex iure, sint corrupti quia enim?</p>
-        </Card>
-        <Card>
-          <h1>Lorem</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id eos libero temporibus. Voluptatem atque ex iure, sint corrupti quia enim?</p>
-        </Card>
-      </div>
-      <Link href="/new">
-        <AccentButton>
-          New Poll
-        </AccentButton>
-      </Link>
-    </div>
+    </>
   );
 }
 
--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -2,13 +2,9 @@
 export default function Privacy() {
   return (
     <>
-      <h1>Privacy</h1>
+      <h1>Demo Deployment</h1>
       <br />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam obcaecati recusandae fuga delectus. Modi consequatur impedit nostrum odio temporibus aspernatur.</p>
-      <br />
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum sapiente ducimus repellat voluptas optio numquam, nostrum recusandae fuga. Sapiente quasi, reiciendis veniam impedit iste doloribus porro. Sapiente consectetur sit labore quos dicta assumenda odit.</p>
-      <br />
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur atque iusto dolore autem, aliquam error quaerat inventore.</p>
+      <p>Please do not input any personal data, only test data.</p>
     </>
   )
 }
